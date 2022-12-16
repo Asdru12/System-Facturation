cea8ea1945a907e4873e14473040f2208b8886dd
Asdrubal Valenzuela <asdruproxx@gmail.com>
2022-12-16 11:15:03 -0500

    Commit de toda la Infraestructura del proyecto

--- systemfacturatioapp/systemfacturatioapp/settings.py
+++ systemfacturatioapp/systemfacturatioapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'systemfacturationfrontend',
+    'sfusuariosapp'
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,21 @@ WSGI_APPLICATION = 'systemfacturatioapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'systemfacturation',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
+""" DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} """
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
